e249d6f2d1f0ff4e4941b904fbd3e9bf3a12329d
Tony-Coniglio <113558159+Tony-Coniglio@users.noreply.github.com>
2022-12-01 16:54:05 -0600

    file setup

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+
+                // My name is Tony Coniglio. I was born and raised in Austin, Texas. I have a BA in History from St. Edward's
+                // university in Austin, Texas, and a MA in History from Texas State University in San Marcos, Texas. I worked
+                // for 10 years in the petro-chemical industry
+                // specifically in print-grade films and polymers. While working in polymers I held positions ranging from
+                // intern and janitor to sales, procurement, and logistics.
+
+                // I have also worked in interior design, with a specific focus on logistics and white-glove services. During
+                // my time in interior design I founded and grew an in-house white-glove moving and delivery service as a new
+                // entity/sister company.
